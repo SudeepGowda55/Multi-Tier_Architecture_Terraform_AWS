6bd5c27b40c6ab59debd0954ac2ecc9ea1e40f23
SudeepGowda55 <ssudeepgowda55@gmail.com>
2022-10-23 12:36:54 +0530

    string

--- subnets.tf
+++ subnets.tf
@@ -20,21 +20,27 @@ resource "aws_subnet" "3tier_public_subnet1" {
   }
 }
 
-resource "aws_subnet" "3tier_public_subnet2" {
-  cidr_block = "10.0.1.0/24"
-  vpc_id = aws_vpc.multitier_vpc.id
-  availability_zone = "us-east-1b"
-  tags = {
-    Name = "3tier_public_subnet2"
-  }
-}
+# resource "aws_subnet" "3tier_public_subnet2" {
+#   cidr_block = "10.0.1.0/24"
+#   vpc_id = aws_vpc.multitier_vpc.id
+#   availability_zone = "us-east-1b"
+#   tags = {
+#     Name = "3tier_public_subnet2"
+#   }
+# }
 
 
-resource "aws_subnet" "3tier_private_subnet" {
-  vpc_id = aws_vpc.multitier_vpc.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "us-east-1c"
-  tags = {
-    Name = "3tier_private_subnet"
-  }
-}
+# resource "aws_subnet" "3tier_private_subnet" {
+#   vpc_id = aws_vpc.multitier_vpc.id
+#   cidr_block = "10.0.2.0/24"
+#   availability_zone = "us-east-1c"
+#   tags = {
+#     Name = "3tier_private_subnet"
+#   }
+# }
+
+# data "aws_subnets" "subnets_id" {
+#   filter {
+    
+#   }
+# }

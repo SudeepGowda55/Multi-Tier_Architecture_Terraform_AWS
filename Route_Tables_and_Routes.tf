6281b78a6eaad78702e2c897d3953869ef690210
SudeepGowda55 <ssudeepgowda55@gmail.com>
2022-11-07 12:45:21 +0530

    some additions

--- Route_Tables_and_Routes.tf
+++ Route_Tables_and_Routes.tf
@@ -33,14 +33,14 @@ resource "aws_route_table_association" "multi_tier_route_table_association2" {
   ]
 }
 
-# resource "aws_default_route_table" "default-route_table" {
-#   default_route_table_id = aws_vpc.multi-tier_vpc.default_route_table_id
+resource "aws_default_route_table" "default-route_table" {
+  default_route_table_id = aws_vpc.multi-tier_vpc.default_route_table_id
   
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     nat_gateway_id = aws_nat_gateway.multiTier_nat_gateway.id
-#   }
-#   tags = {
-#     "Name" = "nat_route"
-#   }
-# }
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.multiTier_nat_gateway.id
+  }
+  tags = {
+    "Name" = "nat_gateway_route"
+  }
+}
